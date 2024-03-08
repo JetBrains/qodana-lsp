90d0feb85e1602fc2811726403c3e6a377d029b4
Evgenii Im <evgenii.im@jetbrains.com>
2024-03-08 13:33:47 +0200

    Simplify initialization/shutdown.

--- src/main/kotlin/org/jetbrains/qodana/SarifLanguageServer.kt
+++ src/main/kotlin/org/jetbrains/qodana/SarifLanguageServer.kt
@@ -1,20 +1,23 @@
 package org.jetbrains.qodana
 
-import org.apache.logging.log4j.LogManager
-import org.eclipse.lsp4j.*
-import org.eclipse.lsp4j.services.LanguageClient
-import org.eclipse.lsp4j.services.LanguageClientAware
-import java.util.concurrent.CompletableFuture
-import kotlin.system.exitProcess
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
 import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.merge
+import kotlinx.coroutines.launch
+import org.apache.logging.log4j.LogManager
+import org.eclipse.lsp4j.*
+import org.eclipse.lsp4j.services.LanguageClient
+import org.eclipse.lsp4j.services.LanguageClientAware
 import org.jetbrains.annotations.VisibleForTesting
 import java.nio.file.Path
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.system.exitProcess
 
 @OptIn(FlowPreview::class)
 class SarifLanguageServer: ExtendedLanguageServer, LanguageClientAware {
@@ -23,7 +26,10 @@ class SarifLanguageServer: ExtendedLanguageServer, LanguageClientAware {
     }
 
     private val workspaceService = SarifWorkspaceService()
-    private var clientCapabilities: ClientCapabilities? = null
+    private val serverCapabilities = ServerCapabilities().apply {
+        setTextDocumentSync(TextDocumentSyncKind.Incremental)
+        diagnosticProvider = DiagnosticRegistrationOptions()
+    }
     private val scope = CoroutineScope(Dispatchers.IO)
     private val requestChannel = Channel<IRequest>(UNLIMITED)
     private val debouncedChannel = Channel<IRequest>(UNLIMITED)
@@ -33,7 +39,6 @@ class SarifLanguageServer: ExtendedLanguageServer, LanguageClientAware {
         System.getProperty("os.name").lowercase().contains("win"),
         scope)
     private val textDocumentService = SarifTextDocumentService(state)
-    private var exitCode = -1
     init {
         scope.launch {
             merge(requestChannel.consumeAsFlow(), debouncedChannel.consumeAsFlow().debounce(DebounceInterval)).collect {
@@ -75,6 +80,7 @@ class SarifLanguageServer: ExtendedLanguageServer, LanguageClientAware {
     }
 
     override fun closeReport(): CompletableFuture<Unit> {
+        logger.info("server/closeReport")
         val future = CompletableFuture<Unit>()
 
         scope.launch {
@@ -90,14 +96,7 @@ class SarifLanguageServer: ExtendedLanguageServer, LanguageClientAware {
 
     override fun initialize(params: InitializeParams?): CompletableFuture<InitializeResult> {
         logger.info("server/initialize")
-        return CompletableFuture.supplyAsync {
-            val capabilities = ServerCapabilities().apply {
-                setTextDocumentSync(TextDocumentSyncKind.Incremental)
-                diagnosticProvider = DiagnosticRegistrationOptions()
-            }
-            clientCapabilities = params?.capabilities
-            InitializeResult(capabilities)
-        }
+        return CompletableFuture.completedFuture(InitializeResult(serverCapabilities))
     }
 
     override fun setTrace(params: SetTraceParams?) {
@@ -106,14 +105,14 @@ class SarifLanguageServer: ExtendedLanguageServer, LanguageClientAware {
 
     override fun shutdown(): CompletableFuture<Any> {
         logger.info("server/shutdown")
-        return CompletableFuture.supplyAsync {
-
-        }
+        requestChannel.close()
+        debouncedChannel.close()
+        return CompletableFuture.completedFuture(null)
     }
 
     override fun exit() {
         logger.info("server/exit")
-        exitProcess(exitCode)
+        exitProcess(0)
     }
 
     override fun connect(client: LanguageClient?) {
