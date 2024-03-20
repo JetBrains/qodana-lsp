dea7621e791e3cc2d1e927fe6eb864d7d89e5d06
Evgenii Im <evgenii.im@jetbrains.com>
2024-03-20 12:59:17 +0200

    Fix token tests after changing behavior (now we prefill token window with value from env or secrets)

--- vscode/qodana/src/test/suite/token.test.ts
+++ vscode/qodana/src/test/suite/token.test.ts
@@ -26,32 +26,7 @@ describe('obtainToken', () => {
     sandbox.restore();
   });
 
-  it('1: should return the token from the environment variable', async () => {
-    const fakeToken = 'envToken';
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    sandbox.stub(process, 'env').value({ QODANA_TOKEN: fakeToken });
-    sandbox.stub(vscode.window, 'showInputBox').resolves(undefined);
-
-    const token = await obtainToken(context);
-    assert.strictEqual(token, fakeToken);
-    sinon.assert.notCalled(secrets.get as sinon.SinonStub);
-    sinon.assert.notCalled(vscode.window.showInputBox as sinon.SinonStub);
-  });
-
-  it('2: should return the token from the secret storage', async () => {
-    const fakeToken = 'secretToken';
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    sandbox.stub(process, 'env').value({ QODANA_TOKEN: undefined });
-    sandbox.stub(vscode.window, 'showInputBox').resolves(undefined);
-    (secrets.get as sinon.SinonStub).resolves(fakeToken);
-
-    const token = await obtainToken(context);
-    assert.strictEqual(token, fakeToken);
-    sinon.assert.calledOnceWithExactly(secrets.get as sinon.SinonStub, QODANA_TOKEN);
-    sinon.assert.notCalled(vscode.window.showInputBox as sinon.SinonStub);
-  });
-
-  it('3: should prompt the user for a token if it is not set', async () => {
+  it('1: should prompt the user for a token if it is not set', async () => {
     const fakeToken = 'userToken';
     // eslint-disable-next-line @typescript-eslint/naming-convention
     sandbox.stub(process, 'env').value({ QODANA_TOKEN: undefined });
@@ -65,7 +40,7 @@ describe('obtainToken', () => {
     sinon.assert.calledOnceWithExactly(secrets.store as sinon.SinonStub, QODANA_TOKEN, fakeToken);
   });
 
-  it('4: should not store the token if the user cancels the input prompt', async () => {
+  it('2: should not store the token if the user cancels the input prompt', async () => {
     // eslint-disable-next-line @typescript-eslint/naming-convention
     sandbox.stub(process, 'env').value({ QODANA_TOKEN: undefined });
     (secrets.get as sinon.SinonStub).resolves(undefined);
