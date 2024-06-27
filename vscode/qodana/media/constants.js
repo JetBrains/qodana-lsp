8ce901b4629b8e2234c359821dc9d4f32bbee1d2
Evgenii Im <evgenii.im@jetbrains.com>
2024-06-27 12:23:23 +0200

    Add missing constants file to fix UI

---
+++ vscode/qodana/media/constants.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+(function () {
+    window.myConstants = {
+        COMMAND_LOG_IN: 'qodana.login',
+        COMMAND_LOG_IN_CUSTOM_SERVER: 'qodana.loginCustomServer',
+        COMMAND_LOG_OUT: 'qodana.logout',
+        COMMAND_CLOSE_REPORT: 'qodana.closeReport',
+        COMMAND_RUN_LOCALLY: 'qodana.runLocally',
+        COMMAND_OPEN_LOCAL_REPORT: 'qodana.openLocalReport'
+    };
+}());
